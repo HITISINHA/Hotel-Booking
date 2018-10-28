fb6cc510f4a985fe29e876b038f86143c67f5906
HITISINHA <sinha.hiti@gmail.com>
2018-10-28 18:24:07 +0530

    Complete UI Changes

--- RouteHandler.js
+++ RouteHandler.js
@@ -43,8 +43,8 @@ exports.display_login=function(url, request, response){
 					console.log("error");
 					//response.end("Invalid User");
 					response.writeHead(200, {'Content-Type': 'text/html'});
-					response.write("<body bgcolor='#E2C2F6'><center>Invalid User try login Again!!</center></body>");
-					response.write("<center><a href='home'>Back to Login</a></center>");
+					response.write("<body bgcolor='#E2C2F6'><center class='wrap-content wrap-login100'>Invalid User try login Again!!</center></body>");
+					response.write("<center class='wrap-content wrap-login100'><a href='home'>Back to Login</a></center>");
 					response.end();
 				}            
 
@@ -93,8 +93,8 @@ exports.display_register= function (url, request, response){
 				if(password==confirmpassword){
 					result=module.addUser(name,password,address,phoneno,email,response);
 					response.writeHeader(200, {"Content-Type": "text/html"}); 
-					response.write("<body bgcolor='#E2C2F6'><center>"+result+"</center></body>");
-					response.write("<center><a href='home'>Click here to Login</a></center>");
+					response.write(" <head><title></title><style>.container-login100{width: 100%; min-height: 100vh;display: -webkit-box;display: -webkit-flex;display: -moz-box;display: -ms-flexbox;display: flex;flex-wrap: wrap;justify-content: center;align-items: center;padding: 15px;background: #9053c7;background: -webkit-linear-gradient(-180deg, #4dad13, #4181d0);background: -o-linear-gradient(-180deg, #4dad13, #4181d0);background: -moz-linear-gradient(-180deg, #4dad13, #4181d0);background: linear-gradient(-180deg, #4dad13, #4181d0);}.wrap-login100{margin: 20px;width: max-content;background:none;border-radius: 10px;overflow: hidden;display: -webkit-box;display: -webkit-flex;display: -moz-box;display: -ms-flexbox;display: flex;flex-wrap: wrap;justify-content: space-between;padding: 0;padding-top: 0px;padding-right: 0px;padding-bottom: 0px;padding-left: 0px;}.wrap-content{padding:0.3%}.login100-form-title{font-family: Poppins-Bold;font-size: 24px;color: #ffffff;line-height: 1.2;text-align: center;width: 100%;display: block;padding-bottom: 54px;}.label{font-family: Poppins-Bold;font-size: 20px;color: #333333;line-height: 1.2;width: 100%;display: block;padding-bottom: 54px;padding-left: 22px;}</style> </head><body class='container-login100'><center class='wrap-content wrap-login100'><div class='login100-form-title'>"+result);
+					response.write("<br><a href='home'>Click here to Login</a></div></center></body>");
 					fs.readFile('./user_details.json', 'utf-8', function(err, data) {
 						if (err) throw err
 						var arrayOfObjects = JSON.parse(data)
@@ -111,8 +111,8 @@ exports.display_register= function (url, request, response){
 				else{
 
 					response.writeHead(200, {'Content-Type': 'text/html'});
-					response.write("<body bgcolor='#E2C2F6'><center>Password does not match with confirm password!!</center></body>");
-					response.write("<center><a href='signup'>Try again</a></center>");
+					response.write(" <head><title></title><style>.container-login100{width: 100%; min-height: 100vh;display: -webkit-box;display: -webkit-flex;display: -moz-box;display: -ms-flexbox;display: flex;flex-wrap: wrap;justify-content: center;align-items: center;padding: 15px;background: #9053c7;background: -webkit-linear-gradient(-180deg, #4dad13, #4181d0);background: -o-linear-gradient(-180deg, #4dad13, #4181d0);background: -moz-linear-gradient(-180deg, #4dad13, #4181d0);background: linear-gradient(-180deg, #4dad13, #4181d0);}.wrap-login100{margin: 20px;width: max-content;background:none;border-radius: 10px;overflow: hidden;display: -webkit-box;display: -webkit-flex;display: -moz-box;display: -ms-flexbox;display: flex;flex-wrap: wrap;justify-content: space-between;padding: 0;padding-top: 0px;padding-right: 0px;padding-bottom: 0px;padding-left: 0px;}.wrap-content{padding:0.3%}.login100-form-title{font-family: Poppins-Bold;font-size: 24px;color: #ffffff;line-height: 1.2;text-align: center;width: 100%;display: block;padding-bottom: 54px;}.label{font-family: Poppins-Bold;font-size: 20px;color: #333333;line-height: 1.2;width: 100%;display: block;padding-bottom: 54px;padding-left: 22px;}</style> </head><body class='container-login100'><center class='wrap-content wrap-login100'><div class='login100-form-title'>Password does not match with confirm password!!");
+					response.write("<br><a href='signup'>Try again</a></div></center></body>");
 					response.end();             
 				}
 
@@ -140,8 +140,8 @@ exports.display_bookroom= function (url, request, response){
 		if(roomtype){
 			result=module.addBooking(adults,children,roomtype);
 			response.writeHeader(200, {"Content-Type": "text/html"}); 
-			response.write("<body bgcolor='#E2C2F6'><center>"+result+"</center></body>");
-			response.write("<center><a href='home'>Go to HomePage</a></center>");
+			response.write(" <head><title></title><style>.container-login100{width: 100%; min-height: 100vh;display: -webkit-box;display: -webkit-flex;display: -moz-box;display: -ms-flexbox;display: flex;flex-wrap: wrap;justify-content: center;align-items: center;padding: 15px;background: #9053c7;background: -webkit-linear-gradient(-180deg, #4dad13, #4181d0);background: -o-linear-gradient(-180deg, #4dad13, #4181d0);background: -moz-linear-gradient(-180deg, #4dad13, #4181d0);background: linear-gradient(-180deg, #4dad13, #4181d0);}.wrap-login100{margin: 20px;width: max-content;background:none;border-radius: 10px;overflow: hidden;display: -webkit-box;display: -webkit-flex;display: -moz-box;display: -ms-flexbox;display: flex;flex-wrap: wrap;justify-content: space-between;padding: 0;padding-top: 0px;padding-right: 0px;padding-bottom: 0px;padding-left: 0px;}.wrap-content{padding:0.3%}.login100-form-title{font-family: Poppins-Bold;font-size: 24px;color: #ffffff;line-height: 1.2;text-align: center;width: 100%;display: block;padding-bottom: 54px;}.label{font-family: Poppins-Bold;font-size: 20px;color: #333333;line-height: 1.2;width: 100%;display: block;padding-bottom: 54px;padding-left: 22px;}</style> </head><body class='container-login100'><center class='wrap-content wrap-login100'><div class='login100-form-title'>"+result);
+			response.write("<br><a href='home'>Go to HomePage</a></div></center></body>");
 			fs.readFile('./booking_details.json', 'utf-8', function(err, data) {
 				if (err) throw err
 				var arrayOfObjects = JSON.parse(data)
@@ -159,8 +159,8 @@ exports.display_bookroom= function (url, request, response){
 		else{
 
 			response.writeHead(200, {'Content-Type': 'text/html'});
-			response.write("<body bgcolor='#E2C2F6'><center>Booking could not be confirmed!</center></body>");
-			response.write("<center><a href='signup'>Try again</a></center>");
+			response.write(" <head><title></title><style>.container-login100{width: 100%; min-height: 100vh;display: -webkit-box;display: -webkit-flex;display: -moz-box;display: -ms-flexbox;display: flex;flex-wrap: wrap;justify-content: center;align-items: center;padding: 15px;background: #9053c7;background: -webkit-linear-gradient(-180deg, #4dad13, #4181d0);background: -o-linear-gradient(-180deg, #4dad13, #4181d0);background: -moz-linear-gradient(-180deg, #4dad13, #4181d0);background: linear-gradient(-180deg, #4dad13, #4181d0);}.wrap-login100{margin: 20px;width: max-content;background:none;border-radius: 10px;overflow: hidden;display: -webkit-box;display: -webkit-flex;display: -moz-box;display: -ms-flexbox;display: flex;flex-wrap: wrap;justify-content: space-between;padding: 0;padding-top: 0px;padding-right: 0px;padding-bottom: 0px;padding-left: 0px;}.wrap-content{padding:0.3%}.login100-form-title{font-family: Poppins-Bold;font-size: 24px;color: #ffffff;line-height: 1.2;text-align: center;width: 100%;display: block;padding-bottom: 54px;}.label{font-family: Poppins-Bold;font-size: 20px;color: #333333;line-height: 1.2;width: 100%;display: block;padding-bottom: 54px;padding-left: 22px;}</style> </head><body class='container-login100'><center class='wrap-content wrap-login100'><div class='login100-form-title'>Booking could not be confirmed!");
+			response.write("<br><a href='home'>Try again</a></div></center></body>");
 			response.end();             
 		}
 
@@ -208,7 +208,7 @@ exports.getImageResponse=function(request,response){
 
 				break;
 
-				case '/hotel-tariff':  img=fs.readFileSync('./hotel-images/hotel-tariff.jpg');
+				case '/hotel-tariff.jpg':  img=fs.readFileSync('./hotel-images/hotel-tariff.jpg');
 
 				break;
 
